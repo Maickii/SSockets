f271968efd3cdb2e9e4600a01f32cb8045993803
Michael <Michael@Home.home>
2020-03-20 01:13:36 -0400

    Fix travis test with new api

--- tests/new_api/test.sh
+++ tests/new_api/test.sh
@@ -1,3 +1,3 @@
-python tests/new_api/server_recv_then_send.py &
+python3 tests/new_api/server_recv_then_send.py &
 sleep 2
-python tests/new_api/client_send_then_recv.py
+python3 tests/new_api/client_send_then_recv.py
