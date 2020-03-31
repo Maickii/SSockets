81e484740a18d6a6b925ec62e6444b3136b3b849
lilybyan <56975133+lilybyan@users.noreply.github.com>
2020-03-31 19:55:29 -0400

    Update ssockets.py. The following changes have been made:  * Additional argument to "client" class, ECDH default, RSA option  * Added RSA library  * Fixed syntax errors  * Additional argument to server, ECDH default

--- ssockets.py
+++ ssockets.py
@@ -16,28 +16,36 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 
 class server:
-	def __init__(self, host, port):
+	def __init__(self, host, port, alg = "ecdh"):
 		self.__host = host
 		self.__port = port
-		self.__server_private_key = ec.generate_private_key(ec.SECP384R1(), default_backend())
-		self.__server_public_key = self.__server_private_key.public_key()
-		self.__socket = self.__bind_socket()
-		self.__conn, self.__addr = self.__connect_to_client(self.__socket)
-		self.__client_public_key = self.__server_exchange_keys(self.__conn, self.__server_public_key)
-		if self.__client_public_key:
-			#we have the child's public key. now we can perform the exchange and ready to receive encrypted data
-			self.__shared_key = self.__server_private_key.exchange(ec.ECDH(), self.__client_public_key)
-		self.__derived_key = HKDF(
-			algorithm=hashes.SHA256(),
-			length=32,
-			salt=None,
-			info=b'handshake data',
-			backend=default_backend()
-		).derive(self.__shared_key)
-		self.__f = Fernet(base64.urlsafe_b64encode(self.__derived_key))
+		if alg == "ecdh":
+			self.__server_private_key = ec.generate_private_key(ec.SECP384R1(), default_backend())
+			self.__server_public_key = self.__server_private_key.public_key()
+			self.__socket = self.__bind_socket()
+			self.__conn, self.__addr = self.__connect_to_client(self.__socket)
+			self.__client_public_key = self.__server_exchange_keys(self.__conn, self.__server_public_key)
+			if self.__client_public_key:
+				#we have the child's public key. now we can perform the exchange and ready to receive encrypted data
+				self.__shared_key = self.__server_private_key.exchange(ec.ECDH(), self.__client_public_key)
+			self.__derived_key = HKDF(
+				algorithm=hashes.SHA256(),
+				length=32,
+				salt=None,
+				info=b'handshake data',
+				backend=default_backend()
+			).derive(self.__shared_key)
+			self.__f = Fernet(base64.urlsafe_b64encode(self.__derived_key))
+		elif alg == "rsa":
+			self.__server_private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048, backend=default_backend())
+			self.__server_public_key = self.__server_private_key.public_key()
+			self.__conn, self.__addr = self.__connect_to_client(self.__socket)
+			self.__client_public_key = self.__server_exchange_keys(self.__conn, self.__server_public_key)
+	# end def __init__
 
 	def __bind_socket(self):
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -68,25 +76,36 @@ class server:
 		self.__conn.sendall(encrypted_data)
 
 class client:
-	def __init__(self, host, port):
+	def __init__(self, host, port, alg = "ecdh"):
 		self.__host = host
 		self.__port = port
-		self.__client_private_key = ec.generate_private_key(ec.SECP384R1(), default_backend())
-		self.__client_public_key = self.__client_private_key.public_key()
-		self.__connected_socket = self.__connect_to_server()
-		self.__server_public_key = self.__client_exchange_keys(self.__connected_socket, self.__client_public_key)
-		if self.__server_public_key:
-			self.__shared_key = self.__client_private_key.exchange(ec.ECDH(), self.__server_public_key)
-			self.__derived_key = HKDF(
-				algorithm=hashes.SHA256(),
-				length=32,
-				salt=None,
-				info=b'handshake data',
-				backend=default_backend()
-			).derive(self.__shared_key)
-			self.__f = Fernet(base64.urlsafe_b64encode(self.__derived_key))
-		else:
-			print("could not retrieve the server's public key") #TODO throw an exception
+		# Elliptic curve Diffie-Hellman
+		if alg == "ecdh":
+			self.__client_private_key = ec.generate_private_key(ec.SECP384R1(), default_backend())
+			self.__client_public_key = self.__client_private_key.public_key()
+			self.__connected_socket = self.__connect_to_server()
+			self.__server_public_key = self.__client_exchange_keys(self.__connected_socket, self.__client_public_key)
+			if self.__server_public_key:
+				self.__shared_key = self.__client_private_key.exchange(ec.ECDH(), self.__server_public_key)
+				self.__derived_key = HKDF(
+					algorithm=hashes.SHA256(),
+					length=32,
+					salt=None,
+					info=b'handshake data',
+					backend=default_backend()
+				).derive(self.__shared_key)
+				self.__f = Fernet(base64.urlsafe_b64encode(self.__derived_key))
+			else:
+				print("could not retrieve the server's public key") #TODO throw an exception
+		# RSA encryption
+		elif alg == "rsa":
+			# generate client private key
+			self.__client_private_key = rsa.generate_private_key(public_exponent=65537, key_size=2048, backend=default_backend())
+			# generate client public key from client private key
+			self.__client_public_key = self.__client_private_key.public_key()
+			self.__connected_socket = self.__connect_to_server()
+			self.__server_public_key = self.__client_exchange_keys(self.__connected_socket, self.__client_public_key)
+	# end def __init__
 
 	def __connect_to_server(self):
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
