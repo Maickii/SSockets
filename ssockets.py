ca508810d20a15bb86b22e1f6e0c7ddf291299f4
lilybyan <56975133+lilybyan@users.noreply.github.com>
2020-03-31 20:04:08 -0400

    Update ssockets.py
    Added RSA library

--- ssockets.py
+++ ssockets.py
@@ -16,6 +16,7 @@ from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 
 class server:
