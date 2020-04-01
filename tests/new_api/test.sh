0088c4c16e34adb0a664dc1d0c5a9a9d47be91cb
LilyByan <liliana.byan@gmail.com>
2020-04-01 13:51:39 -0400

    Revert changes

--- tests/new_api/test.sh
+++ tests/new_api/test.sh
@@ -1,7 +1,3 @@
 python3 tests/new_api/server_recv_then_send.py &
 sleep 2
 python3 tests/new_api/client_send_then_recv.py
-sleep 2
-python3 tests/new_api/ecdh_server.py &
-sleep 2
-python3 tests/new_api/ecdh_client.py
