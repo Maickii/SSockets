b4635e60e6409f949f6e17c74746844542a09619
LilyByan <liliana.byan@gmail.com>
2020-04-01 20:45:01 -0400

    update ecdh

--- crypto_custom/ecdh_client_test.py
+++ crypto_custom/ecdh_client_test.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.abspath('.'))
 from ssockets import client
 
 myclient = client("127.0.0.1", 60000, "ecdh")
-data = b"Hey, whats up server? it's me, the client talking, I'm using a elipitic curve key!"
+data = b"It's me, the client talking, I'm using a elipitic curve key!"
 print("[CLIENT] Sending the following data to the server: \"" + str(data, 'utf-8') + "\"")
 myclient.send(data)
 data = myclient.recv()
--- crypto_custom/ecdh_server_test.py
+++ crypto_custom/ecdh_server_test.py
@@ -7,6 +7,6 @@ from ssockets import server
 myserver = server("127.0.0.1", 60000, "ecdh")
 data = myserver.recv()
 print("[SERVER] The client sent the following data: \"" + str(data, 'utf-8') + "\"")
-data = b"Hey, whats up client? it's me, the server talking, I'm using an ellipitc curve key!"
+data = b"It's me, the server talking, I'm using an ellipitc curve key!"
 print("[SERVER] Sending the following data to the client: \"" + str(data, 'utf-8') + "\"")
 myserver.send(data)
