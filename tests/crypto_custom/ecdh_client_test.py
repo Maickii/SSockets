e0cd78a0a51f1493a023bab7ab5ca28a54ba13d9
lilybyan <56975133+lilybyan@users.noreply.github.com>
2020-04-17 21:40:16 -0400

    Update ecdh_client_test.py

--- tests/crypto_custom/ecdh_client_test.py
+++ tests/crypto_custom/ecdh_client_test.py
@@ -5,7 +5,6 @@ sys.path.append(os.path.abspath('.'))
 from ssockets import client
 
 myclient = client("127.0.0.1", 65001,False,False, "ecdh")
-print(dir(myclient))
 data = b"It's me, the client talking, I'm using a elipitic curve key!"
 print("[CLIENT] Sending the following data to the server: \"" + str(data, 'utf-8') + "\"")
 myclient.send(data)
