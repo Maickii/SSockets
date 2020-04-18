d902e756ecd31246ee3a4f41dacb26ffdf7a4b56
lilybyan <56975133+lilybyan@users.noreply.github.com>
2020-04-17 21:40:41 -0400

    Update client_send_then_recv.py

--- tests/new_api/client_send_then_recv.py
+++ tests/new_api/client_send_then_recv.py
@@ -5,7 +5,6 @@ sys.path.append(os.path.abspath('.'))
 from ssockets import client
 
 myclient = client("127.0.0.1", 60000)
-print(dir(myclient))
 data = b"Hey, whats up server? it's me, the client talking!"
 print("[CLIENT] Sending the following data to the server: \"" + str(data, 'utf-8') + "\"")
 myclient.send(data)
